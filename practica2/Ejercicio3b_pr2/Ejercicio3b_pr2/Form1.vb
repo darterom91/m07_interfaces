370a19e685b29e2ea02cbe0cf666f11999969ec2
David Artero <d.arterom@gmail.com>
2017-10-05 01:04:27 +0200

    Practica2 ejercicio3b_pr2

---
+++ practica2/Ejercicio3b_pr2/Ejercicio3b_pr2/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim numero As Integer = Me.TextBox1.Text
+        Dim Respuesta As String
+
+        Respuesta = If(numero > 0, "Positivo", "Negativo")
+        Me.TextBox2.Text = Respuesta
+    End Sub
+End Class
