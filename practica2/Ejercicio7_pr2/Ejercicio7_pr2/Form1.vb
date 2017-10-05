73c53a30d6a564264a391a765ab99cfac5c8d04a
David Artero <d.arterom@gmail.com>
2017-10-05 14:19:56 +0200

    Practica 2: Ejercicio7

---
+++ practica2/Ejercicio7_pr2/Ejercicio7_pr2/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim suma, valor, i As Integer
+        i = 0
+        Dim Respuesta As String = ""
+
+        Do
+            valor = InputBox("Introduce un numero", "numeros capturados:")
+            Me.TextBox1.Text = Me.TextBox1.Text & valor & vbCrLf
+            suma = suma + valor
+            If i = 10 Then
+                Respuesta = InputBox("Respuesta", "Introduce la  respuesta")
+
+            End If
+            i = i + 1
+        Loop While (Respuesta <> "no")
+        Me.TextBox2.Text = suma
+    End Sub
+End Class
