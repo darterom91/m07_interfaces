be9a88b777ff2f94ea0a1df5340523e43744c94e
David Artero <d.arterom@gmail.com>
2017-11-13 14:00:21 +0100

    practica 11

---
+++ UF1/practica10/Ejercicio1_pr10/Ejercicio1_pr10/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim num As Integer = Me.TextBox1.Text
+        Dim i, j, res As Integer
+
+        Me.DataGridView1.ColumnCount = 3
+        Me.DataGridView1.Columns(0).Name = "Multiplicad"
+        Me.DataGridView1.Columns(1).Name = "Multiplicador"
+        Me.DataGridView1.Columns(2).Name = "Resultat"
+
+        For i = 0 To 10
+            res = i * num
+            Me.DataGridView1.Rows.Add(num, i, res)
+        Next
+    End Sub
+
+End Class
