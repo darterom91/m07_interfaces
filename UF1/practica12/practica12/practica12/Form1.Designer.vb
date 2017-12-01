41e6c0e25371851620a2988ccbe1e7de98b7dd2f
David Artero <d.arterom@gmail.com>
2017-12-01 13:44:51 +0100

    Practica 12

---
+++ UF1/practica12/practica12/practica12/Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.X = New System.Windows.Forms.Label()
+        Me.Y = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(48, 33)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'X
+        '
+        Me.X.AutoSize = True
+        Me.X.Location = New System.Drawing.Point(65, 263)
+        Me.X.Name = "X"
+        Me.X.Size = New System.Drawing.Size(20, 13)
+        Me.X.TabIndex = 1
+        Me.X.Text = "X :"
+        '
+        'Y
+        '
+        Me.Y.AutoSize = True
+        Me.Y.Location = New System.Drawing.Point(194, 263)
+        Me.Y.Name = "Y"
+        Me.Y.Size = New System.Drawing.Size(20, 13)
+        Me.Y.TabIndex = 2
+        Me.Y.Text = "Y :"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(220, 260)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(70, 20)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(91, 260)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(70, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Location = New System.Drawing.Point(68, 59)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(262, 152)
+        Me.Panel1.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(393, 319)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Y)
+        Me.Controls.Add(Me.X)
+        Me.Name = "Form1"
+        Me.Text = "Eventos del mouse"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents X As System.Windows.Forms.Label
+    Friend WithEvents Y As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+
+End Class
---
+++ UF1/practica12/practica12/practica12/Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+    Dim selection As Boolean
+    Dim mv_boton As Boolean
+
+    Private Sub Button1_MouseLeave(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button1.MouseLeave
+        Button1.BackColor = Color.White
+        Button1.ForeColor = Color.Black
+        Button1.Text = "Button1"
+    End Sub
+
+    Private Sub Button1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Button1.MouseMove
+        Button1.Text = "Move"
+        Button1.BackColor = Color.Yellow
+        Button1.ForeColor = Color.Blue
+    End Sub
+
+    Private Sub Button1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Button1.MouseDown
+        Button1.Font = New Font("Microsoft Sans Serif", 20)
+        Button1.Size = New Size(100, 100)
+
+        Button1.Text = "Down"
+        Button1.BackColor = Color.Magenta
+        Button1.ForeColor = Color.Aqua
+
+        If selection = False Then
+            selection = True
+            mv_boton = True
+        Else
+            selection = False
+            mv_boton = False
+        End If
+    End Sub
+
+    Private Sub panel1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Panel1.MouseMove
+        If mv_boton = True Then
+            Button1.Top = Fix(e.Y)
+            TextBox1.Text = Button1.Top
+
+            Button1.Left = Fix(e.X)
+            TextBox2.Text = Button1.Left
+        End If
+    End Sub
+
+    Private Sub Button1_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Button1.MouseUp
+        Button1.Font = New Font("Microsoft Sans Serif", 10)
+        Button1.Text = "Up"
+        Button1.BackColor = Color.White
+        Button1.ForeColor = Color.Black
+        Button1.Size = New Size(75, 23)
+    End Sub
+End Class
